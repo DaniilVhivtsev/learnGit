7cdff205371b73ff42108f254e78fea97ea1335b
Daniil <75034877+DaniilVhivtsev@users.noreply.github.com>
2022-02-04 21:35:35 +0500

    Second commit

---
+++ src/com/test/Main.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        var symbol = (char) System.in.read();
+
+        System.out.println(symbol);
+    }
+}
